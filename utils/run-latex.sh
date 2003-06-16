a7b052b275ed3930c6bb5fdf37230e360c6eb71a
K. Handa <handa@gnu.org>
2003-06-16 22:05:10 +0900

    *** empty log message ***

--- utils/run-latex.sh
+++ utils/run-latex.sh
@@ -4,13 +4,19 @@
 
 umask 002
 CURDIR=`pwd`
-TEXINPUTS=$CURDIR/$1/styles:$CURDIR/$1/data-$2:.:
+TEXINPUTS=${CURDIR}/$1/styles:${CURDIR}/$1/data-$2:.:
 export TEXINPUTS
 cd $2/latex
-platex refman.tex
+if [ "$2" = "ja" ] ; then
+  LATEX=platex
+  PAPER='-t a4'
+else
+  LATEX=latex
+fi
+${LATEX} refman.tex
 if [ "$2" = "ja" ] ; then
   nkf -e < refman.idx > temp.idx; mv temp.idx refman.idx
 fi
 makeindex refman.idx
-platex refman.tex
-dvips -t a4 -o refman.ps refman.dvi
+${LATEX} refman.tex
+dvips ${PAPER} -o refman.ps refman.dvi
