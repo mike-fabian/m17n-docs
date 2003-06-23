502bb8fad78b94383c8e2533b173ad9758c08983
K. Handa <handa@gnu.org>
2003-06-23 15:39:31 +0900

    *** empty log message ***

--- data-usr/mainpage.c
+++ data-usr/mainpage.c
@@ -1,13 +1,34 @@
 /***en
-    @mainpage My main page.
+    @mainpage The m17n library documentation
 
-    @section sect1 The first section.
+    @section sect1 Background
 
-    section contents.
+    What is the multilingual text processing?
 
-    @section sect2 The second section.
+    Why is the m17n library necessary?
 
-    section contents.
+    @section sect2 What is the m17n library?
+
+    The @e m17n @e library is a multilingual text processing library
+    for the C language.  It provides following facilities to handle
+    multilingual text.
+
+    <ul>
+    <li> @e M-text: A data structure for multilingual text objects.
+
+    <li> Functions for creating and processing M-texts.
+
+    <li> Decoders and encoders for converting M-texts from/to strings
+    encoded in various existing formats.
+
+    <li> A huge character space, which contains all the Unicode
+    characters and more non-Unicode characters.
+
+    <li> @e Chartable: A data structure that contains per-character
+    information effectively.
+
+    <li> Functions for inputting and displaying M-text.
+    </ul>
 
   */
 ////
