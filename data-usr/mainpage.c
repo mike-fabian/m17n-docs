7d509118eaf25096966cfe901af5432678975ed2
K. Handa <handa@gnu.org>
2003-06-23 10:11:40 +0900

    *** empty log message ***

---
+++ data-usr/mainpage.c
@@ -0,0 +1,14 @@
+/***en
+    @mainpage My main page.
+
+    @section sect1 The first section.
+
+    section contents.
+
+    @section sect2 The second section.
+
+    section contents.
+
+  */
+////
+
