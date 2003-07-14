2b8df39f25cfee882b29ddb014042323a3a1d66b
K. Handa <handa@gnu.org>
2003-07-14 12:46:19 +0900

    *** empty log message ***

---
+++ utils/latex.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# $1: target directory (usr/latex, ja/latex, or dev/latex)
+# $2: latex commnad (latex, platex, or pdflatex)
+
+umask 002
+USR_JA_DEV=`dirname $1`
+TEXINPUTS=`pwd`/styles:`pwd`/data-${USR_JA_DEV}:.:
+export TEXINPUTS
+cd $1
+LATEX=$2
+PAPER='-t a4'
+${LATEX} m17n-lib.tex
+if [ "${USR_JA_DEV}" = "ja" ] ; then
+  nkf -e < m17n-lib.idx > temp.idx; mv temp.idx m17n-lib.idx
+fi
+makeindex m17n-lib.idx
+${LATEX} m17n-lib.tex
+if [ "$LATEX" != "pdflatex" ] ; then
+  dvips ${PAPER} -o m17n-lib.ps m17n-lib.dvi
+fi
