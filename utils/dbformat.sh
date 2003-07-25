c6fd61407815afac4b44ba807d5690d24c6dba06
K. Handa <handa@gnu.org>
2003-07-25 21:36:40 +0900

    *** empty log message ***

--- utils/dbformat.sh
+++ utils/dbformat.sh
@@ -3,14 +3,20 @@
 cat <<EOF
 /***en @page m17nDBFormat Data format of the m17n database
 
-This section describes the data formats of the m17n database. */
+This section describes formats of these data supplied by the m17n
+database.
 
+<ul>
 EOF
 
+cat $* | grep '@page' | sed -e 's/@page mdb\([^ ]*\)/<li> @ref mdb\1 "\1" --/'
+
+cat <<EOF
+</ul>
+*/
+EOF
+
+cat $* | \
 sed -n -e '/\/\*\*\*/,/\*\// p' | \
 sed -e 's/@section/@subsection/' \
     -e 's/@page/@section/'
-
-cat <<EOF
-////
-EOF
--- utils/example.sh
+++ utils/example.sh
@@ -3,15 +3,20 @@
 cat <<EOF
 /***en @page m17nExample Example Programs
 
-This section describes the example programs.  They are to demonstrate
-the usage of the m17n library, not for practical use. */
+This section describes these example programs.  They are to demonstrate
+the usage of the m17n library, not for practical use.
 
+<ul>
 EOF
 
+cat $* | grep '@page' | sed -e 's/@page \([^ ]*\)/<li> @ref \1 "\1" --/'
+
+cat <<EOF
+</ul>
+*/
+EOF
+
+cat $* | \
 sed -n -e '/\/\*\*\*/,/\*\// p' | \
 sed -e 's/@section/@subsection/' \
     -e 's/@page \([^ ]*\)/@section \1 \1 --/'
-
-cat <<EOF
-////
-EOF
