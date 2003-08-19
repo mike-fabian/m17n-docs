783b8e277e05e78e3ce59e98a4997883d4dbb1fd
K. Handa <handa@gnu.org>
2003-08-19 09:21:21 +0900

    *** empty log message ***

--- utils/db-flt.sh
+++ utils/db-flt.sh
@@ -3,19 +3,21 @@
 cat <<EOF
 /***en @page m17nDBFLT Font Layout Tables provided by the m17n databasse
 
-Here's a list of font layout tables provided by the m17n databasse and
-their brief descriptions.  */
+Here's a list of font layout tables (FLT) provided by the m17n
+databasse and their brief descriptions.  */
 
 /***ja @page m17nDBFLT M17N データベースが提供する FLT
 
-M17N データベースが提供する FLT (Font Layout Table) のリストとそれぞれ
+M17N データベースが提供する Font Layout Table (FLT) のリストとそれぞれ
 の簡単な説明。 */
 
 /*** <ul> */
 EOF
 
 for file in $*; do
-  sed -n '/BEG-DOXYGEN/,/END-DOXYGEN/s/^;; //p' < $file
+  echo "/***"
+  sed -n '/^;; <li>/,/^$/p' < $file
+  echo "*/"
 done
 
 cat <<EOF
