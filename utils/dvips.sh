d58dd3a9d08854364779bfb1ef566dd587b42ecd
K. Handa <handa.kenichi@aist.go.jp>
2023-06-29 11:04:55 +0900

    * utils/dvips.sh: Fix the output file name to m17n-lib.ps.

--- utils/dvips.sh
+++ utils/dvips.sh
@@ -3,16 +3,8 @@
 # $2: dvips commnad (usually dvips)
 
 umask 002
-USR_JA_DEV=`dirname $1`
-if [ $USR_JA_DEV = "ja" ] ; then
-  SOURCE=m17n-lib-ja
-elif [ $USR_JA_DEV = "dev" ] ; then
-  SOURCE=m17n-lib-dev
-else
-  SOURCE=m17n-lib
-fi
 export TEXINPUTS
 cd $1
 DVIPS=$2
-${DVIPS} -o ${SOURCE}.ps ${SOURCE}.dvi
+${DVIPS} -o m17n-lib.ps m17n-lib.dvi
 
